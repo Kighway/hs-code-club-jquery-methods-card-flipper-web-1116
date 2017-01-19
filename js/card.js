3c9cb5e04c8007dfee0888618e6b6e8fb1112aa0
Kyle Tulod <tulod.kyle.m@gmail.com>
2017-01-19 17:27:34 -0500

    Done.

--- js/card.js
+++ js/card.js
@@ -1,11 +1,16 @@
 $( function(){
 
-  $( "#card-one").click(function(){
-    //Use toggleClass method here
-  });
-
+  $("#card-one").click(function(){
+    $("#card-one").toggleClass("flipped")
+  })
 
 	// code click event for card-two here
+  $("#card-two").click(function(){
+    $("#card-two").toggleClass("flipped")
+  })
 
   // code click event for card-three here
+  $("#card-three").click(function(){
+    $("#card-three").toggleClass("flipped")
+  })
  })
